74c8d06b50ffdf9c29ac0c5072b13f5bf35d56a7
Ian Crosby <iancrosby@Ians-MacBook-Pro.local>
2016-01-13 16:54:48 +0100

    Adding Mantl config

---
+++ mantl-conf/gce.sample.tf
@@ -0,0 +1,21 @@
+provider "google" {
+  project = "container-solutions-workshops"
+  region = "europe-west1"
+  credentials = "${file("account.json")}"
+  account_file = "${file("account.json")}"
+}
+
+module "gce-dc" {
+  source = "./terraform/gce"
+  datacenter = "gce-dc"
+  control_type = "n1-standard-1"
+  worker_type = "n1-standard-2"
+  network_ipv4 = "10.0.0.0/16"
+  long_name = "microservices-infrastructure"
+  short_name = "mi"
+  region = "europe-west1"
+  zone = "europe-west1-b"
+  control_count = 1
+  worker_count = 3
+  edge_count = 1
+}
