a20c76ec86770235c046ca6db5a53cea2ffca733
Adrian Mouat <adrian.mouat@gmail.com>
2016-01-15 10:39:19 +0000

    Add drone domain.

--- mantl-conf/gce.sample.tf
+++ mantl-conf/gce.sample.tf
@@ -13,9 +13,12 @@ module "gce-dc" {
   network_ipv4 = "10.0.0.0/16"
   long_name = "microservices-infrastructure"
   short_name = "mi"
+  domain = "container-solutions.com"
+  subdomain = ".drone"
   region = "europe-west1"
   zone = "europe-west1-b"
   control_count = 1
   worker_count = 3
   edge_count = 1
 }
+
